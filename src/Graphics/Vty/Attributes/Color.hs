3a6a9b09a8ab1bacf00258226806b573a75de762
ShrykeWindgrace <shryke.windgrace@gmail.com>
2023-02-27 10:48:42 +0100

    Hardcode FullColor support
    WIP

--- src/Graphics/Vty/Attributes/Color.hs
+++ src/Graphics/Vty/Attributes/Color.hs
@@ -51,7 +51,6 @@ import GHC.Generics
 import Control.DeepSeq
 import System.Environment (lookupEnv)
 
-import qualified System.Console.Terminfo as Terminfo
 import Control.Exception (catch)
 import Data.Maybe
 
@@ -120,21 +119,12 @@ data ColorMode
     | FullColor
     deriving ( Eq, Show )
 
+
+-- I know that my terminal has support for FullColor
+-- I do not quite know how to handle ColorMode240
+-- and that does not seem like a priority right now
 detectColorMode :: String -> IO ColorMode
-detectColorMode termName' = do
-    term <- catch (Just <$> Terminfo.setupTerm termName')
-                  (\(_ :: Terminfo.SetupTermError) -> return Nothing)
-    let getCap cap = term >>= \t -> Terminfo.getCapability t cap
-        termColors = fromMaybe 0 $ getCap (Terminfo.tiGetNum "colors")
-    colorterm <- lookupEnv "COLORTERM"
-    return $ if
-        | termColors <  8               -> NoColor
-        | termColors <  16              -> ColorMode8
-        | termColors == 16              -> ColorMode16
-        | termColors <  256             -> ColorMode240 (fromIntegral termColors - 16)
-        | colorterm == Just "truecolor" -> FullColor
-        | colorterm == Just "24bit"     -> FullColor
-        | otherwise                     -> ColorMode240 240
+detectColorMode _ = pure FullColor
 
 black, red, green, yellow, blue, magenta, cyan, white :: Color
 black  = ISOColor 0
